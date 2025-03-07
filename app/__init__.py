18a0ab94f2e4d979640ac660a160b8188277b1a6
MuizZatam <muizzatam110@gmail.com>
2025-03-07 10:20:20 +0000

    Add comments to revise

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
+# importing main flask class - Flask
 from flask import Flask
 
+# instantiates the application
 app = Flask(__name__)
 
+# imports routes module from app package
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
+# render template is used to map a html template
+# with a view function
 from flask import render_template
+
+# imports application instance from __init__.py
 from app import app
 
 
+# route decorators map web app urls to a view function
+# this function's logic is executed when the associated
+# route is visited
 @app.route("/")
 @app.route("/index")
 def index():
@@ -20,4 +27,7 @@ def index():
         }
     ]
 
+    # the render_template takes an arbitary number of arguments
+    # along with a template name that serve as the outgoing
+    # data to the view
     return render_template('index.html', title = "Home", user = user, posts = posts)
